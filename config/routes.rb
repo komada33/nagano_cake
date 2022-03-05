ccb13bb1384c8242fa28cf5f912b94f1cc2d656c
EC2 Default User <ec2-user@ip-172-31-22-2.ec2.internal>
2022-03-05 15:10:11 +0000

    admin 画面骨組み作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,7 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'orders/show'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/ready'
-    get 'orders/thanks'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/check'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-    get 'addresses/creste'
-    get 'addresses/update'
-    get 'addresses/dstroy'
-  end
   devise_for :customers,skip: [:password], controllers: {
   registrations: "public/registrations",
-  sessions: 'public/sessions'
+  sessions: 'public/sessions',
 }
 
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
--- db/migrate/20220302135130_add_column_to_customers.rb
+++ db/migrate/20220302135130_add_column_to_customers.rb
@@ -7,6 +7,6 @@ class AddColumnToCustomers < ActiveRecord::Migration[6.1]
     add_column :customers, :postal_code, :string
     add_column :customers, :address, :string
     add_column :customers, :telephone_number, :string
-    add_column :customers, :is_deleted, :boolean
+    add_column :customers, :is_deleted, :boolean, default: false
   end
 end
