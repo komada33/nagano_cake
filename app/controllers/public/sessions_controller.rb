a9a51ab2c9825ac75e6822798b4f7050dce0a5da
EC2 Default User <ec2-user@ip-172-31-22-2.ec2.internal>
2022-03-12 08:44:30 +0000

    ジャンル　途中

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -24,7 +24,7 @@ class Public::SessionsController < Devise::SessionsController
     def customer_state
       @customer = Customer.find_by(email: params[:customer][:email])
       return if !@customer
-      if @customer.vaild_password?(params[:customer][:password])
+      if @customer.valid_password?(params[:customer][:password])
        true && !false
       end
     end
@@ -32,4 +32,12 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+    def after_sign_in_path_for(resource)
+      root_path
+    end
+
+    def after_sign_out_path_for(resource)
+      root_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
 
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions",
-
 }
 
 root 'public/homes#top'
