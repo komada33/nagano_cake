13b08c0f84c08fe50cb551737bc4a3a1d5b3c46a
EC2 Default User <ec2-user@ip-172-31-22-2.ec2.internal>
2022-03-12 14:30:04 +0000

    admin/customer　途中

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,16 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:customer_id, :last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_deleted)
   end
 end
